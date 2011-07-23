52683a39b33851b7fc3a661bc00827088bda40bf
Hendrik Leppkes <h.leppkes@gmail.com>
2011-07-23 18:28:38 +0200

    Added ffdshow custom code for h264 random access improvements.

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -1354,6 +1354,12 @@ typedef struct AVFrame {
      */
     int64_t pkt_pos;
 
+    /* ffdshow custom code (begin) */
+    int h264_poc_decoded;
+    int h264_poc_outputed;
+    int h264_frame_num_decoded;
+    int h264_max_frame_num;
+     /* ffdshow custom code (end) */
 } AVFrame;
 
 struct AVCodecInternal;
