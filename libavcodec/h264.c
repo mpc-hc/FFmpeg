742813b7c9763056740a31b01b3e3c4ec4c44d1f
Hendrik Leppkes <h.leppkes@gmail.com>
2011-07-29 17:44:14 +0200

    h264: Set avctx->pix_fmt after codec init to the proper value.

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -1072,6 +1072,18 @@ av_cold int ff_h264_decode_init(AVCodecContext *avctx){
         ff_h264_decode_extradata(h))
         return -1;
 
+    switch (h->sps.bit_depth_luma) {
+        case 9 :
+            avctx->pix_fmt = CHROMA444 ? PIX_FMT_YUV444P9 : PIX_FMT_YUV420P9;
+            break;
+        case 10 :
+            avctx->pix_fmt = CHROMA444 ? PIX_FMT_YUV444P10 : PIX_FMT_YUV420P10;
+            break;
+        default:
+            avctx->pix_fmt = CHROMA444 ? PIX_FMT_YUV444P : PIX_FMT_YUV420P;
+            break;
+    }
+
     if(h->sps.bitstream_restriction_flag && s->avctx->has_b_frames < h->sps.num_reorder_frames){
         s->avctx->has_b_frames = h->sps.num_reorder_frames;
         s->low_delay = 0;
