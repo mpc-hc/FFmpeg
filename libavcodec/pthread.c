435df71cce29bc01dca7aaf5bcadd8dafee07da0
Hendrik Leppkes <h.leppkes@gmail.com>
2012-01-04 19:27:58 +0100

    HACK: Disable automatic multi-threading.
    Using multi-threading for stream probing is a bad idea.

--- libavcodec/pthread.c
+++ libavcodec/pthread.c
@@ -158,7 +158,7 @@ typedef struct FrameThreadContext {
 
 /* H264 slice threading seems to be buggy with more than 16 threads,
  * limit the number of threads to 16 for automatic detection */
-#define MAX_AUTO_THREADS 16
+#define MAX_AUTO_THREADS 1
 
 static int get_logical_cpus(AVCodecContext *avctx)
 {
