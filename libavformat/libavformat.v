a562a4f5cf85c9060a798e04c164e2da0ee62d02
Hendrik Leppkes <h.leppkes@gmail.com>
2012-02-09 20:40:02 +0100

    Export avformat functions used in LAV

--- libavformat/libavformat.v
+++ libavformat/libavformat.v
@@ -1,6 +1,10 @@
 LIBAVFORMAT_MAJOR {
     global:
         av*;
+        #for LAV Audio/Video
+        ff_sipr_subpk_size;
+        ff_rm_reorder_sipr_data;
+        ff_vorbis_comment;
         #FIXME those are for ffserver
         ff_inet_aton;
         ff_socket_nonblock;
