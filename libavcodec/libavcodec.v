c5b5f58d908a704a73741a5d4473038422699b3b
Hendrik Leppkes <h.leppkes@gmail.com>
2012-02-28 20:07:30 +0100

    Export avcodec functions used in LAV

--- libavcodec/libavcodec.v
+++ libavcodec/libavcodec.v
@@ -3,5 +3,8 @@ LIBAVCODEC_$MAJOR {
                 #deprecated, remove after next bump
                 audio_resample;
                 audio_resample_close;
+                #LAV usage
+                ff_vc1_pixel_aspect;
+                ff_crop_tab;
         local:  *;
 };
