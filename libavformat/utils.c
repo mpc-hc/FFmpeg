15ab46bfc0f53e73249353e8b8536d11f62a74ec
Michael Niedermayer <michaelni@gmx.at>
2012-03-07 21:41:54 +0100

    avformat_new_stream: fix initial last_dts value
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -2927,6 +2927,7 @@ AVStream *avformat_new_stream(AVFormatContext *s, AVCodec *c)
         av_free(st);
         return NULL;
     }
+    st->info->last_dts = AV_NOPTS_VALUE;
 
     st->codec = avcodec_alloc_context3(c);
     if (s->iformat) {
