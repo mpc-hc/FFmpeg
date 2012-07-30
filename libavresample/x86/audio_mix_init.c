f7f2665266dc9ce9b2ff847538cbdbd3b597be7a
Hendrik Leppkes <h.leppkes@gmail.com>
2012-07-30 21:25:31 +0200

    avr: disable 8 -> 2 optimized mixing on 32-bit because of a crash

--- libavresample/x86/audio_mix_init.c
+++ libavresample/x86/audio_mix_init.c
@@ -210,6 +210,8 @@ av_cold void ff_audio_mix_init_x86(AudioMix *am)
     SET_MIX_3_8_TO_1_2(5)
     SET_MIX_3_8_TO_1_2(6)
     SET_MIX_3_8_TO_1_2(7)
+#if !ARCH_X86_32
     SET_MIX_3_8_TO_1_2(8)
+#endif
 #endif /* HAVE_YASM */
 }
