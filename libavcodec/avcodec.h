37e59399b32fe2a6740d5f634d5b59b588af9f2c
Hendrik Leppkes <h.leppkes@gmail.com>
2013-03-30 15:35:06 +0100

    matroskadec_haali: send new extradata and stream params on segment changes

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -1427,6 +1427,9 @@ enum AVPacketSideDataType {
      * if (param_flags & AV_SIDE_DATA_PARAM_CHANGE_DIMENSIONS)
      *     s32le width
      *     s32le height
+     * if (param_flags & AV_SIDE_DATA_PARAM_CHANGE_ASPECTRATIO)
+     *     s32le num
+     *     s32le den
      * @endcode
      */
     AV_PKT_DATA_PARAM_CHANGE,
@@ -1700,6 +1703,7 @@ enum AVSideDataParamChangeFlags {
     AV_SIDE_DATA_PARAM_CHANGE_CHANNEL_LAYOUT = 0x0002,
     AV_SIDE_DATA_PARAM_CHANGE_SAMPLE_RATE    = 0x0004,
     AV_SIDE_DATA_PARAM_CHANGE_DIMENSIONS     = 0x0008,
+    AV_SIDE_DATA_PARAM_CHANGE_ASPECTRATIO    = 0x8000,
 };
 /**
  * @}
