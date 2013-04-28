f7e1373ac807a9b007e7598b292416751ac6c879
Hendrik Leppkes <h.leppkes@gmail.com>
2013-04-28 12:19:31 +0200

    HACK avio: add a flag to skip the fstat on file opening for performance reasons

--- libavformat/file.c
+++ libavformat/file.c
@@ -227,7 +227,7 @@ static int file_open(URLContext *h, const char *filename, int flags)
         return AVERROR(errno);
     c->fd = fd;
 
-    h->is_streamed = !fstat(fd, &st) && S_ISFIFO(st.st_mode);
+    h->is_streamed = !(flags & AVIO_FLAG_AVOID_FSTAT) && !fstat(fd, &st) && S_ISFIFO(st.st_mode);
 
     /* Buffer writes more than the default 32k to improve throughput especially
      * with networked file systems */
