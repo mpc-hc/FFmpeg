e6b030126d536b3e878c365d4515e5a27ae586fb
Hendrik Leppkes <h.leppkes@gmail.com>
2013-07-26 10:31:05 +0200

    lavfi: add attribute_align_arg to all public entry points
    Previously it was partly only added to central functions called
    internally, however if GCC would inline these into the public fuction, the
    alignment attribute would not take effect.
    
    Instead, add it to all public entry points to avoid these problems.

--- libavfilter/buffersink.c
+++ libavfilter/buffersink.c
@@ -118,7 +118,7 @@ static int filter_frame(AVFilterLink *link, AVFrame *frame)
     return 0;
 }
 
-int av_buffersink_get_frame(AVFilterContext *ctx, AVFrame *frame)
+int attribute_align_arg av_buffersink_get_frame(AVFilterContext *ctx, AVFrame *frame)
 {
     return av_buffersink_get_frame_flags(ctx, frame, 0);
 }
