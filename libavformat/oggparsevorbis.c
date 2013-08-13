4cd63f5f2f283fa13c4b1cdf5ecc944cb1118586
Hendrik Leppkes <h.leppkes@gmail.com>
2013-08-13 13:47:34 +0200

    vorbiscomment: allow using vorbiscomment parser without input context

--- libavformat/oggparsevorbis.c
+++ libavformat/oggparsevorbis.c
@@ -146,7 +146,7 @@ int ff_vorbis_comment(AVFormatContext *as, AVDictionary **m,
                     av_freep(&ct);
                     continue;
                 }
-                if ((ret = av_base64_decode(pict, ct, vl)) > 0)
+                if (as && (ret = av_base64_decode(pict, ct, vl)) > 0)
                     ret = ff_flac_parse_picture(as, pict, ret);
                 av_freep(&tt);
                 av_freep(&ct);
@@ -155,7 +155,7 @@ int ff_vorbis_comment(AVFormatContext *as, AVDictionary **m,
                     av_log(as, AV_LOG_WARNING, "Failed to parse cover art block.\n");
                     continue;
                 }
-            } else if (!ogm_chapter(as, tt, ct))
+            } else if (!as || !ogm_chapter(as, tt, ct))
                 av_dict_set(m, tt, ct,
                             AV_DICT_DONT_STRDUP_KEY |
                             AV_DICT_DONT_STRDUP_VAL);
