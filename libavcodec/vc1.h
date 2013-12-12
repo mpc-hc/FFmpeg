820b739c587ae3d2b3e8488fcedc61db73cad4fa
Hendrik Leppkes <h.leppkes@gmail.com>
2013-12-12 19:54:39 +0100

    vc1: wait for bitstream recovery before starting decoding

--- libavcodec/vc1.h
+++ libavcodec/vc1.h
@@ -391,6 +391,8 @@ typedef struct VC1Context{
 
     int parse_only;              ///< Context is used within parser
     int resync_marker;           ///< could this stream contain resync markers
+
+    int recovered;
 } VC1Context;
 
 /**
