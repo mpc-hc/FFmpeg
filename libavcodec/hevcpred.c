3ec58c95924d78b65ef6de4ceaf5f2c18cebf35d
Hendrik Leppkes <h.leppkes@gmail.com>
2014-08-18 09:44:50 +0200

    hevc: port intra pred SIMD from OpenHEVC

--- libavcodec/hevcpred.c
+++ libavcodec/hevcpred.c
@@ -77,4 +77,6 @@ void ff_hevc_pred_init(HEVCPredContext *hpc, int bit_depth)
 
     if (ARCH_MIPS)
         ff_hevc_pred_init_mips(hpc, bit_depth);
+    if (ARCH_X86)
+        ff_hevc_pred_init_x86(hpc, bit_depth);
 }
