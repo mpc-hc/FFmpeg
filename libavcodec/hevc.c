366ca4be28b7e6dac6f769027171ca26fcfadd48
Hendrik Leppkes <h.leppkes@gmail.com>
2014-09-25 13:35:57 +0200

    hevc: fill avctx information from sps during codec open

--- libavcodec/hevc.c
+++ libavcodec/hevc.c
@@ -3430,7 +3430,7 @@ static int hevc_decode_extradata(HEVCContext *s)
 static av_cold int hevc_decode_init(AVCodecContext *avctx)
 {
     HEVCContext *s = avctx->priv_data;
-    int ret;
+    int ret, i;
 
     ff_init_cabac_states();
 
@@ -3460,6 +3460,27 @@ static av_cold int hevc_decode_init(AVCodecContext *avctx)
             s->threads_type = FF_THREAD_FRAME;
         else
             s->threads_type = FF_THREAD_SLICE;
+
+    if (s->pps == NULL) {
+        for (i = 0; i < MAX_PPS_COUNT; i++) {
+            if (s->pps_list[i] != NULL) {
+                HEVCPPS *pps = (HEVCPPS*)s->pps_list[i]->data;
+                if (s->sps_list[pps->sps_id] == NULL)
+                    continue;
+
+                HEVCSPS *sps = (HEVCSPS*)s->sps_list[pps->sps_id]->data;
+
+                /* set active pps/sps */
+                s->pps = pps;
+                set_sps(s, sps, AV_PIX_FMT_NONE);
+
+                /* additional profile/level info */
+                s->avctx->profile = sps->ptl.general_ptl.profile_idc;
+                s->avctx->level   = sps->ptl.general_ptl.level_idc;
+                break;
+            }
+        }
+    }
 
     return 0;
 }
