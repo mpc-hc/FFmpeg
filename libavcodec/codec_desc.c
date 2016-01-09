db968e4d57aaaf47b5d80075551d9823d9dce34f
Hendrik Leppkes <h.leppkes@gmail.com>
2016-01-09 16:34:09 +0100

    avcodec: add h264_mvc codec id and profiles

--- libavcodec/codec_desc.c
+++ libavcodec/codec_desc.c
@@ -1528,6 +1528,13 @@ static const AVCodecDescriptor codec_descriptors[] = {
         .long_name = NULL_IF_CONFIG_SMALL("Cineform HD"),
         .props     = AV_CODEC_PROP_LOSSY,
     },
+    {
+        .id        = AV_CODEC_ID_H264_MVC,
+        .type      = AVMEDIA_TYPE_VIDEO,
+        .name      = "h264_mvc",
+        .long_name = NULL_IF_CONFIG_SMALL("H264 MVC"),
+        .props     = AV_CODEC_PROP_LOSSY,
+    },
 
     /* various PCM "codecs" */
     {
