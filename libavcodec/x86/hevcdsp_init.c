f5e2e03a4c05b772d8dfadddaf9281c052b61352
Hendrik Leppkes <h.leppkes@gmail.com>
2016-02-12 00:04:19 +0100

    hevc: disable intrapred intrinsics on msvc
    Those two don't like each other very much.

--- libavcodec/x86/hevcdsp_init.c
+++ libavcodec/x86/hevcdsp_init.c
@@ -1142,6 +1142,7 @@ void ff_hevc_pred_init_x86(HEVCPredContext *hpc, int bit_depth)
 {
     int mm_flags = av_get_cpu_flags();
 
+#ifndef _MSC_VER
     if (bit_depth == 8) {
         if (EXTERNAL_SSE4(mm_flags)) {
             HEVC_PRED(8);
@@ -1152,4 +1153,5 @@ void ff_hevc_pred_init_x86(HEVCPredContext *hpc, int bit_depth)
             HEVC_PRED(10);
         }
     }
+#endif
 }
