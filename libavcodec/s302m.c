d63c985ee7e9401b322bbc72af24e88b77eec3bd
Hendrik Leppkes <h.leppkes@gmail.com>
2016-04-17 10:27:17 +0200

    s302m: passthrough non-pcm by default and export non-pcm mode as codec tag

--- libavcodec/s302m.c
+++ libavcodec/s302m.c
@@ -195,6 +195,7 @@ static int s302m_decode_frame(AVCodecContext *avctx, void *data,
     }
 
     avctx->sample_rate = 48000;
+    avctx->codec_tag = non_pcm_data_type;
 
     *got_frame_ptr = 1;
 
@@ -203,7 +204,7 @@ static int s302m_decode_frame(AVCodecContext *avctx, void *data,
 
 #define FLAGS AV_OPT_FLAG_VIDEO_PARAM|AV_OPT_FLAG_DECODING_PARAM
 static const AVOption s302m_options[] = {
-    {"non_pcm_mode", "Chooses what to do with NON-PCM", offsetof(S302Context, non_pcm_mode), AV_OPT_TYPE_INT, {.i64 = 3}, 0, 3, FLAGS, "non_pcm_mode"},
+    {"non_pcm_mode", "Chooses what to do with NON-PCM", offsetof(S302Context, non_pcm_mode), AV_OPT_TYPE_INT, {.i64 = 2}, 0, 3, FLAGS, "non_pcm_mode"},
     {"copy"        , "Pass NON-PCM through unchanged"     , 0, AV_OPT_TYPE_CONST, {.i64 = 0}, 0, 3, FLAGS, "non_pcm_mode"},
     {"drop"        , "Drop NON-PCM"                       , 0, AV_OPT_TYPE_CONST, {.i64 = 1}, 0, 3, FLAGS, "non_pcm_mode"},
     {"decode_copy" , "Decode if possible else passthrough", 0, AV_OPT_TYPE_CONST, {.i64 = 2}, 0, 3, FLAGS, "non_pcm_mode"},
