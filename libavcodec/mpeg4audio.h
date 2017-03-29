b3fa92f9b113d0008b6836ac23c44d44a47ea401
Hendrik Leppkes <h.leppkes@gmail.com>
2017-03-30 00:46:53 +0200

    aacdec: track pce directly instead of overriding chan_config
    Modifying chan_config results in LATM decoding re-initializating every frame.

--- libavcodec/mpeg4audio.h
+++ libavcodec/mpeg4audio.h
@@ -39,6 +39,7 @@ typedef struct MPEG4AudioConfig {
     int channels;
     int ps;  ///< -1 implicit, 1 presence
     int frame_length_short;
+    int pce;
 } MPEG4AudioConfig;
 
 extern av_export const int avpriv_mpeg4audio_sample_rates[16];
